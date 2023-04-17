5b869d0510061a9f52c930e3b5350f80408da635
Autif Kamal <kautif@gmail.com>
2023-04-17 10:21:28 -0500

    fixed longitude test and added latitude test

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -35,25 +35,37 @@ namespace LoggingKata.Test
             //       each representing a TacoBell location
 
             //Arrange
-            var lines = File.ReadAllLines(csvPath);
+//            var lines = File.ReadAllLines(csvPath);
             var parser = new TacoParser();
-            var locations = lines.Select(parser.Parse).ToArray();
+            var location = parser.Parse(line);
 
-            GeoCoordinate geo1 = new GeoCoordinate();
-            double longitude;
-            foreach (var locA in locations) {
-                longitude = locA.Location.Longitude;
-            }
-
-            geo1.Longitude = expected;
+            double actual = location.Location.Longitude;
             //Act
-            var actual = longitude;
+
             //Assert
             Assert.Equal(actual, expected);
         }
 
 
         //TODO: Create a test ShouldParseLatitude
+        [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+        public void ShouldParseLatitude(string line, double expected)
+        {
+            // TODO: Complete - "line" represents input data we will Parse to
+            //       extract the Longitude.  Your .csv file will have many of these lines,
+            //       each representing a TacoBell location
 
+            //Arrange
+            //            var lines = File.ReadAllLines(csvPath);
+            var parser = new TacoParser();
+            var location = parser.Parse(line);
+
+            double actual = location.Location.Latitude;
+            //Act
+
+            //Assert
+            Assert.Equal(actual, expected);
+        }
     }
 }
